c7d94d3ca07e4625d62d890273e5b44504e3e5e8
fredery07 <frederytherond@hotmail.com>
2026-09-11 23:26:51 +0200

    Load partner carousel and admin manager

--- config.js
+++ config.js
@@ -2,3 +2,12 @@ window.SKIPPERNOW_CONFIG = {
   supabaseUrl: "https://pzvlarwsfvhenrniepkw.supabase.co",
   supabaseAnonKey: "sb_publishable_OxikOn1mhDcxkAVAucN5Lg_za-bJMS5"
 };
+
+(function(){
+  if(window.__skippernowPartnersLoader) return;
+  window.__skippernowPartnersLoader = true;
+  const s = document.createElement("script");
+  s.src = "/partners-v1.js";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
