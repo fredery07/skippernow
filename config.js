b716108c9025786511a186a4a7929c69b779d8f1
fredery07 <frederytherond@hotmail.com>
2026-09-11 23:38:37 +0200

    Load robust partners fix on homepage and admin

--- config.js
+++ config.js
@@ -8,12 +8,17 @@ window.SKIPPERNOW_CONFIG = {
   window.__skippernowPartnersLoader = true;
 
   const partners = document.createElement("script");
-  partners.src = "/partners-v1.js";
+  partners.src = "/partners-v1.js?v=2";
   partners.defer = true;
   document.head.appendChild(partners);
 
   const layout = document.createElement("script");
-  layout.src = "/home-layout-v1.js";
+  layout.src = "/home-layout-v1.js?v=2";
   layout.defer = true;
   document.head.appendChild(layout);
+
+  const fix = document.createElement("script");
+  fix.src = "/partners-fix-v2.js?v=1";
+  fix.defer = true;
+  document.head.appendChild(fix);
 })();
